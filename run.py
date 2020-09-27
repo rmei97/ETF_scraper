a896407c0ba82ef42411b411ac1f9982b55c8f19
Richard Mei <richard.mei97@gmail.com>
2020-09-26 23:18:24 -0400

    read inputs to determine what method to do

--- run.py
+++ run.py
@@ -16,38 +16,48 @@ from functions import *
 # -------------------------------------------- #
 
 #Connect to existing database, here is currently a sqlite db located in same directory
-conn = sqlite3.connect('ETF.db')
-c = conn.cursor()
+def solve():
+    conn = sqlite3.connect('ETF.db')
+    c = conn.cursor()
 
-#erros dict for 
-results = {}
-fail_batch = []
-#look through the database and run a prophet model to find lowest error
-for t in listdir('./data'):
-    ticker, ext = t.split('.')
+    #erros dict for 
+    results = {}
+    fail_batch = []
+    #look through the database and run a prophet model to find lowest error
+    for t in listdir('./data'):
+        ticker, ext = t.split('.')
 
-    if ext != 'csv':
-        continue
-    try:
-        fetch = retrieve(c,ticker)
-        if len(fetch) < 50:
+        if ext != 'csv':
+            continue
+        try:
+            fetch = retrieve(c,ticker)
+            if len(fetch) < 50:
+                fail_batch.append(ticker)
+                continue
+            error, forecast, summary = prophetize(fetch,4)
+            results[ticker] = error
+
+        except TypeError:
             fail_batch.append(ticker)
             continue
-        error, forecast, summary = prophetize(fetch,4)
-        results[ticker] = error
 
-    except TypeError:
-        fail_batch.append(ticker)
-        continue
+        print('-next iter-')
 
-    print('-next iter-')
-
-#find the min error and the resulting key value pair
-min_error = min(results.values())
-out = [key for key in results if results[key] == min_error]
+    #find the min error and the resulting key value pair
+    min_error = min(results.values())
+    out = [key for key in results if results[key] == min_error]
 
 #see the tickers associated with the lowest RMSE
-print('Tickers of interest: ', out)
-print('With the lowest error of: ', min_error)
-print('Failed batches at :', fail_batch)
+    print('Tickers of interest: ', out)
+    print('With the lowest error of: ', min_error)
+    print('Failed batches at :', fail_batch)
+
+
+if __name__ == '__main__':
+    command = input()
+    if command == 'solve':
+        solve()
+    if command == 'exit':
+        exit()
+    #add update database function
 

9fce565a79b7e040c413ed80a318fb23c47c9839
Richard Mei <richard.mei97@gmail.com>
2020-09-29 20:40:53 -0400

    home and run, no css

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 from flask import Flask, render_template,request
+from functions import *
+
+from os import listdir, chdir
+
+import matplotlib.pyplot as plt
+import sqlite3
+import pandas as pd
+import numpy as np
+
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_pacf
+from statsmodels.graphics.tsaplots import plot_acf
+from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.arima_model import ARMA
 
 app = Flask(__name__)
 
@@ -17,19 +31,46 @@ app = Flask(__name__)
 
 
 @app.route("/")
-# def home():
-#     return render_template('home.html')	
+def home():
+    return render_template('home.html')	
 
-def contact():
-    if request.method == 'POST':
-        if request.form['submit_button'] == 'Do Something':
-            render_template('about.html') # do something
-        elif request.form['submit_button'] == 'Do Something Else':
-            print('do something else') # do something else
-        else:
-            print('sometung something else') # unknown
-    elif request.method == 'GET':
-        return render_template('home.html')
+@app.route("/run", methods = ['POST','GET'])
+def run():
+    conn = sqlite3.connect('ETF.db')
+    c = conn.cursor()
+
+    #erros dict for 
+    results = {}
+    fail_batch = []
+    #look through the database and run a prophet model to find lowest error
+    for t in listdir('./data')[:5]:
+        ticker, ext = t.split('.')
+
+        if ext != 'csv':
+            continue
+        try:
+            fetch = retrieve(c,ticker)
+            if len(fetch) < 50:
+                fail_batch.append(ticker)
+                continue
+            error, forecast, summary = prophetize(fetch,4)
+            results[ticker] = error
+
+        except TypeError:
+            fail_batch.append(ticker)
+            continue
+
+        print('-next iter-')
+
+    #find the min error and the resulting key value pair
+    min_error =format(min(results.values()),'.4f')
+    out = [key for key in results if results[key] == min_error]
+
+    return render_template('model.html', out=out, min_error = min_error)
+#see the tickers associated with the lowest RMSE
+    # print('Tickers of interest: ', out)
+    # print('With the lowest error of: ', min_error)
+    # print('Failed batches at :', fail_batch)
 
 @app.route("/salvador")
 def salvador():
